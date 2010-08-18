4926a9ba312b23b509e2ec9df491235428e3e873
frank.cornelis <frank.cornelis@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2010-08-18 13:26:03 +0000

    initial work on protocol SPI

---
+++ eid-dss-model/src/main/java/be/fedict/eid/dss/model/ProtocolServiceManager.java
@@ -0,0 +1,41 @@
+/*
+ * eID Digital Signature Service Project.
+ * Copyright (C) 2009-2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.dss.model;
+
+import java.util.Map;
+
+import javax.ejb.Local;
+
+/**
+ * Interface for component that manages the registered protocol services.
+ * 
+ * @author Frank Cornelis
+ * 
+ */
+@Local
+public interface ProtocolServiceManager {
+
+	/**
+	 * Gives back a map with (context path, protocol service class name) tuples.
+	 * 
+	 * @return
+	 */
+	Map<String, String> getProtocolServices();
+
+}
---
+++ eid-dss-webapp/src/main/java/be/fedict/eid/dss/webapp/StartupServletContextListener.java
@@ -0,0 +1,75 @@
+/*
+ * eID Digital Signature Service Project.
+ * Copyright (C) 2009-2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.dss.webapp;
+
+import java.util.Map;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import be.fedict.eid.dss.model.ProtocolServiceManager;
+
+/**
+ * Startup servlet component. This servlet context listener boots up the eID DSS
+ * system.
+ * 
+ * @author Frank Cornelis
+ * 
+ */
+public class StartupServletContextListener implements ServletContextListener {
+
+	private static final Log LOG = LogFactory
+			.getLog(StartupServletContextListener.class);
+
+	private static final String PROTOCOL_SERVICES_CONTEXT_ATTRIBUTE = StartupServletContextListener.class
+			.getName()
+			+ ".ProtocolServices";
+
+	@EJB
+	private ProtocolServiceManager protocolServiceManager;
+
+	public void contextInitialized(ServletContextEvent event) {
+		LOG.debug("contextInitialized");
+		ServletContext servletContext = event.getServletContext();
+		/*
+		 * We once load the protocol services so we don't have to iterate over
+		 * all protocol descriptor files upon each DSS request.
+		 */
+		Map<String, String> protocolServices = this.protocolServiceManager
+				.getProtocolServices();
+		servletContext.setAttribute(PROTOCOL_SERVICES_CONTEXT_ATTRIBUTE,
+				protocolServices);
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+		LOG.debug("contextDestroyed");
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> getProtocolServices(ServletContext context) {
+		Map<String, String> protocolServices = (Map<String, String>) context
+				.getAttribute(PROTOCOL_SERVICES_CONTEXT_ATTRIBUTE);
+		return protocolServices;
+	}
+}
