199ba58d08bdff8610c21b2467afa06970ebed6a
frank.cornelis <frank.cornelis@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2010-08-26 09:32:38 +0000

    admin portal: configuration

---
+++ eid-dss-entity/src/main/java/be/fedict/eid/dss/entity/ConfigPropertyEntity.java
@@ -0,0 +1,62 @@
+/*
+ * eID Digital Signature Service Project.
+ * Copyright (C) 2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.dss.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dss_config")
+public class ConfigPropertyEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	private String value;
+
+	public ConfigPropertyEntity() {
+		super();
+	}
+
+	public ConfigPropertyEntity(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	@Id
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
