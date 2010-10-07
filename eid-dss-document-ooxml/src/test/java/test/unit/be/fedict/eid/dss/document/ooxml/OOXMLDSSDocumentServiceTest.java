4117d5d132ba7a3f3923b55e5630fe63b5465cc9
frank.cornelis <frank.cornelis@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2010-10-07 11:24:25 +0000

    OOXML: XAdES-X-L

--- eid-dss-document-ooxml/src/test/java/test/unit/be/fedict/eid/dss/document/ooxml/OOXMLDSSDocumentServiceTest.java
+++ eid-dss-document-ooxml/src/test/java/test/unit/be/fedict/eid/dss/document/ooxml/OOXMLDSSDocumentServiceTest.java
@@ -21,16 +21,23 @@ package test.unit.be.fedict.eid.dss.document.ooxml;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.security.cert.X509CRL;
+import java.security.cert.X509Certificate;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bouncycastle.ocsp.OCSPResp;
+import org.easymock.Capture;
+import org.easymock.EasyMock;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import be.fedict.eid.applet.service.signer.ooxml.OOXMLProvider;
 import be.fedict.eid.dss.document.ooxml.OOXMLDSSDocumentService;
+import be.fedict.eid.dss.spi.DSSDocumentContext;
 import be.fedict.eid.dss.spi.SignatureInfo;
 
 public class OOXMLDSSDocumentServiceTest {
@@ -50,16 +57,38 @@ public class OOXMLDSSDocumentServiceTest {
 		byte[] document = IOUtils.toByteArray(OOXMLDSSDocumentServiceTest.class
 				.getResourceAsStream("/hello-world-signed.docx"));
 
+		DSSDocumentContext mockContext = EasyMock
+				.createMock(DSSDocumentContext.class);
+		Capture<List<X509Certificate>> certificateChainCapture = new Capture<List<X509Certificate>>();
+		Capture<Date> validationDateCapture = new Capture<Date>();
+		Capture<List<OCSPResp>> ocspResponsesCapture = new Capture<List<OCSPResp>>();
+		Capture<List<X509CRL>> crlsCapture = new Capture<List<X509CRL>>();
+		mockContext.validate(EasyMock.capture(certificateChainCapture),
+				EasyMock.capture(validationDateCapture),
+				EasyMock.capture(ocspResponsesCapture),
+				EasyMock.capture(crlsCapture));
+
+		// prepare
+		EasyMock.replay(mockContext);
+
 		// operate
+		testedInstance.init(mockContext, "mime-type");
 		List<SignatureInfo> signatureInfos = testedInstance
 				.verifySignatures(document);
 
 		// verify
+		EasyMock.verify(mockContext);
 		assertNotNull(signatureInfos);
 		assertEquals(1, signatureInfos.size());
 		SignatureInfo signatureInfo = signatureInfos.get(0);
 		assertNotNull(signatureInfo.getSigner());
 		assertNotNull(signatureInfo.getSigningTime());
 		LOG.debug("signing time: " + signatureInfo.getSigningTime());
+		assertEquals(signatureInfo.getSigningTime(),
+				validationDateCapture.getValue());
+		assertEquals(signatureInfo.getSigner(), certificateChainCapture
+				.getValue().get(0));
+		assertEquals(1, ocspResponsesCapture.getValue().size());
+		assertEquals(1, crlsCapture.getValue().size());
 	}
 }
