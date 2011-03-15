083f220d7712d5f19b87d9293b548d13e73080ec
frank.cornelis <frank.cornelis@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2011-03-15 12:45:46 +0000

    remove wrong tagging

--- tags/1.0.0.Beta4/eid-dss-client/src/main/java/be/fedict/eid/dss/client/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains the eID Digital Signature Service client components.
- */
-package be.fedict.eid.dss.client;
-
--- tags/1.0.0.Beta4/eid-dss-protocol-simple-client/src/main/java/be/fedict/eid/dss/protocol/simple/client/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains the eID Digital Signature Service client components for the simple DSS protocol.
- */
-package be.fedict.eid.dss.protocol.simple.client;
-
