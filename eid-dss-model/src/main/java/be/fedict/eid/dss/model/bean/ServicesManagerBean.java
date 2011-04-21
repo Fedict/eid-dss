4bab8f9b22706c8be9f0ff9bc42091b1b5ca5a8f
wim.vandenhaute <wim.vandenhaute@gmail.com>
2011-04-21 14:13:39 +0000

    Show list of supported protocol services on dss main page + add downloadable active DSS identity cert. chain

--- eid-dss-model/src/main/java/be/fedict/eid/dss/model/bean/ServicesManagerBean.java
+++ eid-dss-model/src/main/java/be/fedict/eid/dss/model/bean/ServicesManagerBean.java
@@ -18,64 +18,64 @@
 
 package be.fedict.eid.dss.model.bean;
 
+import be.fedict.eid.applet.service.signer.HttpSessionTemporaryDataStorage;
+import be.fedict.eid.dss.model.DocumentRepository;
+import be.fedict.eid.dss.model.ServicesManager;
+import be.fedict.eid.dss.spi.DSSDocumentService;
+import be.fedict.eid.dss.spi.protocol.DigitalSignatureServiceProtocolType;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import be.fedict.eid.applet.service.signer.HttpSessionTemporaryDataStorage;
-import be.fedict.eid.dss.model.DocumentRepository;
-import be.fedict.eid.dss.model.ServicesManager;
-import be.fedict.eid.dss.spi.DSSDocumentService;
-
 /**
  * Services manager EJB3 bean.
- * 
+ *
  * @author Frank Cornelis
- * 
  */
 @Stateless
 public class ServicesManagerBean implements ServicesManager {
 
-	private static final Log LOG = LogFactory.getLog(ServicesManagerBean.class);
+    private static final Log LOG = LogFactory.getLog(ServicesManagerBean.class);
 
-	@EJB
-	private ServicesManagerSingletonBean servicesManagerSingleton;
+    @EJB
+    private ServicesManagerSingletonBean servicesManagerSingleton;
 
-	public Map<String, String> getProtocolServiceClassNames() {
-		return this.servicesManagerSingleton.getProtocolServiceClassNames();
-	}
+    public Map<String, String> getProtocolServiceClassNames() {
+        return this.servicesManagerSingleton.getProtocolServiceClassNames();
+    }
 
-	public Map<String, String> getDocumentServiceClassNames() {
-		return this.servicesManagerSingleton.getDocumentServiceClassNames();
-	}
+    public List<DigitalSignatureServiceProtocolType> getProtocolServices() {
+        return this.servicesManagerSingleton.getProtocolServices();
+    }
 
-	public DSSDocumentService getDocumentService() {
-		HttpSession httpSession = HttpSessionTemporaryDataStorage
-				.getHttpSession();
-		DocumentRepository documentRepository = new DocumentRepository(
-				httpSession);
-		String contentType = documentRepository.getDocumentContentType();
-		LOG.debug("content type: " + contentType);
-		return this.servicesManagerSingleton.getDocumentService(contentType);
-	}
+    public Map<String, String> getDocumentServiceClassNames() {
+        return this.servicesManagerSingleton.getDocumentServiceClassNames();
+    }
 
-	public DSSDocumentService getDocumentService(String contentType) {
-		return this.servicesManagerSingleton.getDocumentService(contentType);
-	}
+    public DSSDocumentService getDocumentService() {
+        HttpSession httpSession = HttpSessionTemporaryDataStorage
+                .getHttpSession();
+        DocumentRepository documentRepository = new DocumentRepository(
+                httpSession);
+        String contentType = documentRepository.getDocumentContentType();
+        LOG.debug("content type: " + contentType);
+        return this.servicesManagerSingleton.getDocumentService(contentType);
+    }
 
-	public List<String> getSupportedDocumentFormats() {
-		Set<String> documentFormats = this.servicesManagerSingleton
-				.getSupportedDocumentFormats();
-		List<String> documentFormatsList = Collections.list(Collections
-				.enumeration(documentFormats));
-		return documentFormatsList;
-	}
+    public DSSDocumentService getDocumentService(String contentType) {
+        return this.servicesManagerSingleton.getDocumentService(contentType);
+    }
+
+    public List<String> getSupportedDocumentFormats() {
+        Set<String> documentFormats = this.servicesManagerSingleton
+                .getSupportedDocumentFormats();
+        return Collections.list(Collections.enumeration(documentFormats));
+    }
 }
--- eid-dss-webapp-control/src/main/java/be/fedict/eid/dss/control/ServiceEndpoint.java
+++ eid-dss-webapp-control/src/main/java/be/fedict/eid/dss/control/ServiceEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * eID Digital Signature Service Project.
+ * eID Identity Provider Project.
  * Copyright (C) 2010 FedICT.
  *
  * This is free software; you can redistribute it and/or modify it
@@ -12,32 +12,28 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, see 
+ * License along with this software; if not, see
  * http://www.gnu.org/licenses/.
  */
 
 package be.fedict.eid.dss.control;
 
-import javax.ejb.Local;
+public class ServiceEndpoint {
 
-@Local
-public interface ServiceInfo {
+    private final String name;
 
-	/*
-	 * Lifecycle.
-	 */
-	void destroy();
+    private final String path;
 
-	/*
-	 * Factories.
-	 */
-	void initDocumentFormatList();
+    public ServiceEndpoint(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
 
-    void initProtocolServices();
+    public String getName() {
+        return name;
+    }
 
-	void initXmlSchemaNamespacesList();
-
-	void initXmlStyleSheetNamespacesList();
-	
-	public String getServiceFingerprint();
+    public String getPath() {
+        return path;
+    }
 }
