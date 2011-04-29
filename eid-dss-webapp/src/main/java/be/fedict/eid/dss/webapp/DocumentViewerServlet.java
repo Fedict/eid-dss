aaa1be689d2c8ac416be011a230333fddcf96517
wim.vandenhaute <wim.vandenhaute@gmail.com>
2011-04-29 13:51:07 +0000

    Intermediate browserInfo page for gathering browser plugin info, usable by DocumentViewer

--- eid-dss-webapp/src/main/java/be/fedict/eid/dss/webapp/DocumentViewerServlet.java
+++ eid-dss-webapp/src/main/java/be/fedict/eid/dss/webapp/DocumentViewerServlet.java
@@ -18,85 +18,98 @@
 
 package be.fedict.eid.dss.webapp;
 
-import java.io.IOException;
+import be.fedict.eid.dss.control.View;
+import be.fedict.eid.dss.model.DocumentRepository;
+import be.fedict.eid.dss.spi.DSSDocumentService;
+import be.fedict.eid.dss.spi.DocumentVisualization;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import be.fedict.eid.dss.control.View;
-import be.fedict.eid.dss.model.DocumentRepository;
-import be.fedict.eid.dss.spi.DSSDocumentService;
-import be.fedict.eid.dss.spi.DocumentVisualization;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * A servlet for visualizing a document.
- * 
+ *
  * @author Frank Cornelis.
- * 
  */
 public class DocumentViewerServlet extends AbstractProtocolServiceServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final Log LOG = LogFactory
-			.getLog(DocumentViewerServlet.class);
+    private static final Log LOG = LogFactory
+            .getLog(DocumentViewerServlet.class);
 
-	public DocumentViewerServlet() {
-		super(false, true);
-	}
+    public DocumentViewerServlet() {
+        super(false, true);
+    }
 
-	@Override
-	protected void handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException {
-		LOG.debug("doGet");
-		HttpSession httpSession = request.getSession();
-		DocumentRepository documentRepository = new DocumentRepository(
-				httpSession);
-		String contentType = documentRepository.getDocumentContentType();
-		byte[] documentData = documentRepository.getDocument();
+    @Override
+    protected void handleRequest(HttpServletRequest request,
+                                 HttpServletResponse response) throws IOException, ServletException {
 
-		DSSDocumentService documentService = super
-				.findDocumentService(contentType);
-		if (null != documentService) {
-			LOG.debug("document visualization transformation");
-			String language = (String) httpSession
-					.getAttribute(View.LANGUAGE_SESSION_ATTRIBUTE);
-			DocumentVisualization documentVisualization;
-			try {
-				documentVisualization = documentService.visualizeDocument(
-						documentData, language);
-			} catch (Exception e) {
-				throw new ServletException("error visualizing the document: "
-						+ e.getMessage(), e);
-			}
-			if (null != documentVisualization) {
-				contentType = documentVisualization.getBrowserContentType();
-				documentData = documentVisualization.getBrowserData();
-			}
-		}
+        LOG.debug("doGet");
 
-		response.setHeader("Cache-Control",
-				"no-cache, no-store, must-revalidate, max-age=-1"); // http
-																	// 1.1
-		if (false == request.getScheme().equals("https")) {
-			// else the download fails in IE
-			response.setHeader("Pragma", "no-cache"); // http 1.0
-		} else {
-			response.setHeader("Pragma", "public");
-		}
-		response.setDateHeader("Expires", -1);
-		response.setContentLength(documentData.length);
+        // get browser plugins
+        List<String> plugins = BrowserInfoServlet.getPlugins(request.getSession());
+        for (String plugin : plugins) {
+            LOG.debug("Plugin: " + plugin);
+        }
 
-		response.setContentType(contentType);
-		response.setContentLength(documentData.length);
-		ServletOutputStream out = response.getOutputStream();
-		out.write(documentData);
-		out.flush();
-	}
+        // get browser mimeTypes
+        List<MimeType> mimeTypes = BrowserInfoServlet.getMimeTypes(request.getSession());
+        for (MimeType mimeType : mimeTypes) {
+            LOG.debug("MimeTypes: type=" + mimeType.getType() +
+                    " plugin=" + mimeType.getPlugin());
+        }
+
+        HttpSession httpSession = request.getSession();
+        DocumentRepository documentRepository = new DocumentRepository(
+                httpSession);
+        String contentType = documentRepository.getDocumentContentType();
+        byte[] documentData = documentRepository.getDocument();
+
+        DSSDocumentService documentService = super
+                .findDocumentService(contentType);
+        if (null != documentService) {
+            LOG.debug("document visualization transformation");
+            String language = (String) httpSession
+                    .getAttribute(View.LANGUAGE_SESSION_ATTRIBUTE);
+            DocumentVisualization documentVisualization;
+            try {
+                documentVisualization = documentService.visualizeDocument(
+                        documentData, language);
+            } catch (Exception e) {
+                throw new ServletException("error visualizing the document: "
+                        + e.getMessage(), e);
+            }
+            if (null != documentVisualization) {
+                contentType = documentVisualization.getBrowserContentType();
+                documentData = documentVisualization.getBrowserData();
+            }
+        }
+
+        response.setHeader("Cache-Control",
+                "no-cache, no-store, must-revalidate, max-age=-1"); // http
+        // 1.1
+        if (false == request.getScheme().equals("https")) {
+            // else the download fails in IE
+            response.setHeader("Pragma", "no-cache"); // http 1.0
+        } else {
+            response.setHeader("Pragma", "public");
+        }
+        response.setDateHeader("Expires", -1);
+        response.setContentLength(documentData.length);
+
+        response.setContentType(contentType);
+        response.setContentLength(documentData.length);
+        ServletOutputStream out = response.getOutputStream();
+        out.write(documentData);
+        out.flush();
+    }
 }
---
+++ eid-dss-webapp/src/main/webapp/browserInfo.js
@@ -0,0 +1,29 @@
+function detectPlugins() {
+
+    if (navigator.plugins && navigator.plugins.length > 0) {
+
+        var arrayLength = navigator.plugins.length;
+        var plugins = "";
+        for (i=0; i  < arrayLength; i++ ) {
+            plugins += navigator.plugins[i].name + '|';
+        }
+        return plugins;
+    }
+
+    return null;
+}
+function detectMimeTypes() {
+
+    if (navigator.mimeTypes && navigator.mimeTypes.length > 0) {
+
+        var arrayLength = navigator.mimeTypes.length;
+        var mimeTypes = "";
+        for (i=0; i  < arrayLength; i++ ) {
+            mimeTypes += navigator.mimeTypes[i].type + ',' +
+             navigator.mimeTypes[i].enabledPlugin.name + '|';
+        }
+        return mimeTypes;
+    }
+
+    return null;
+}
