842ec96257c0b2796843ae6c72930c7182b2b7a1
wim.vandenhaute@gmail.com <wim.vandenhaute@gmail.com@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2011-05-04 07:41:57 +0000

    Make ConfigurationBean a singleton and keep map of name/value pair config properties in memory for performance reasons

--- trunk/eid-dss-entity/src/main/java/be/fedict/eid/dss/entity/ConfigPropertyEntity.java
+++ trunk/eid-dss-entity/src/main/java/be/fedict/eid/dss/entity/ConfigPropertyEntity.java
@@ -18,56 +18,65 @@
 
 package be.fedict.eid.dss.entity;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.*;
-
 @Entity
 @Table(name = Constants.DATABASE_TABLE_PREFIX + "config")
-@NamedQueries(@NamedQuery(name = ConfigPropertyEntity.LIST_INDEXES,
-        query = "FROM ConfigPropertyEntity WHERE name LIKE :name"))
+@NamedQueries({@NamedQuery(name = ConfigPropertyEntity.LIST_INDEXES,
+        query = "FROM ConfigPropertyEntity WHERE name LIKE :name"),
+        @NamedQuery(name = ConfigPropertyEntity.LIST_ALL,
+                query = "FROM ConfigPropertyEntity")})
 public class ConfigPropertyEntity implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public static final String LIST_INDEXES = "dss.config.list.idx";
+    public static final String LIST_ALL = "dss.config.list.all";
 
-	private String name;
+    private String name;
 
-	private String value;
+    private String value;
 
-	public ConfigPropertyEntity() {
-		super();
-	}
+    public ConfigPropertyEntity() {
+        super();
+    }
 
-	public ConfigPropertyEntity(String name, String value) {
-		this.name = name;
-		this.value = value;
-	}
+    public ConfigPropertyEntity(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
 
-	@Id
-	public String getName() {
-		return this.name;
-	}
+    @Id
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getValue() {
-		return this.value;
-	}
+    public String getValue() {
+        return this.value;
+    }
 
-	public void setValue(String value) {
-		this.value = value;
-	}
+    public void setValue(String value) {
+        this.value = value;
+    }
 
     @SuppressWarnings("unchecked")
     public static List<ConfigPropertyEntity> listConfigsWhereNameLike(
             EntityManager entityManager, String name) {
 
-            return entityManager.createNamedQuery(ConfigPropertyEntity.LIST_INDEXES)
-                    .setParameter("name", "%" + name + "%").getResultList();
+        return entityManager.createNamedQuery(ConfigPropertyEntity.LIST_INDEXES)
+                .setParameter("name", "%" + name + "%").getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<ConfigPropertyEntity> listAll(EntityManager entityManager) {
+
+        return entityManager.createNamedQuery(ConfigPropertyEntity.LIST_ALL)
+                .getResultList();
     }
 }
