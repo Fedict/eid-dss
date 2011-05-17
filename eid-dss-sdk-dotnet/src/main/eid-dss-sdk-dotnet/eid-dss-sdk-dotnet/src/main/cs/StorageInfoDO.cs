77b7747461680b0c9be6c6dc71ae52777fb2f4b9
wim.vandenhaute <wim.vandenhaute@gmail.com>
2011-05-17 14:18:50 +0000

    .NET SDK: WS Client support for "store"/"retrieve" WS operations

---
+++ eid-dss-sdk-dotnet/src/main/eid-dss-sdk-dotnet/eid-dss-sdk-dotnet/src/main/cs/StorageInfoDO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eid_dss_sdk_dotnet
+{
+    public class StorageInfoDO
+    {
+        private String artifact;
+        private DateTime notBefore;
+        private DateTime notAfter;
+
+        public StorageInfoDO(String artifact, DateTime notBefore, DateTime notAfter)
+        {
+            this.artifact = artifact;
+            this.notBefore = notBefore;
+            this.notAfter = notAfter;
+        }
+
+        public String getArtifact()
+        {
+            return this.artifact;
+        }
+
+        public DateTime getNotBefore()
+        {
+            return this.notBefore;
+        }
+
+        public DateTime getNotAfter()
+        {
+            return this.notAfter;
+        }
+    }
+}
