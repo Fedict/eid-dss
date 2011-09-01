ffa1327d28eeec8fe9070f68315ff399e8787ba7
frank.cornelis <frank.cornelis@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2011-09-01 09:57:18 +0000

    Eclipse formatting

--- eid-dss-webapp/src/main/webapp/browserInfo.js
+++ eid-dss-webapp/src/main/webapp/browserInfo.js
@@ -1,31 +1,31 @@
 function detectPlugins() {
 
-    if (navigator.plugins && navigator.plugins.length > 0) {
+	if (navigator.plugins && navigator.plugins.length > 0) {
 
-        var arrayLength = navigator.plugins.length;
-        var plugins = "";
-        for (i=0; i  < arrayLength; i++ ) {
-            plugins += navigator.plugins[i].name + '|';
-        }
-        return plugins;
-    }
+		var arrayLength = navigator.plugins.length;
+		var plugins = "";
+		for (i = 0; i < arrayLength; i++) {
+			plugins += navigator.plugins[i].name + '|';
+		}
+		return plugins;
+	}
 
-    return null;
+	return null;
 }
 function detectMimeTypes() {
 
-    if (navigator.mimeTypes && navigator.mimeTypes.length > 0) {
+	if (navigator.mimeTypes && navigator.mimeTypes.length > 0) {
 
-        var arrayLength = navigator.mimeTypes.length;
-        var mimeTypes = "";
-        for (i=0; i  < arrayLength; i++ ) {
-            if (null != navigator.mimeTypes[i].enabledPlugin) {
-                mimeTypes += navigator.mimeTypes[i].type + ',' +
-                    navigator.mimeTypes[i].enabledPlugin.name + '|';
-            }
-        }
-        return mimeTypes;
-    }
+		var arrayLength = navigator.mimeTypes.length;
+		var mimeTypes = "";
+		for (i = 0; i < arrayLength; i++) {
+			if (null != navigator.mimeTypes[i].enabledPlugin) {
+				mimeTypes += navigator.mimeTypes[i].type + ','
+						+ navigator.mimeTypes[i].enabledPlugin.name + '|';
+			}
+		}
+		return mimeTypes;
+	}
 
-    return null;
+	return null;
 }
