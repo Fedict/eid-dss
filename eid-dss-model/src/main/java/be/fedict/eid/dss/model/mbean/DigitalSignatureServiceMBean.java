d4a717e2023f38e11b77c8ac73a5cf4049a8cdbc
frank.cornelis <frank.cornelis@6783a890-3e0e-11de-8950-77ccfa13e4cc>
2011-10-03 14:44:48 +0000

    OOXMLProvider lifecycle management

--- eid-dss-model/src/main/java/be/fedict/eid/dss/model/mbean/DigitalSignatureServiceMBean.java
+++ eid-dss-model/src/main/java/be/fedict/eid/dss/model/mbean/DigitalSignatureServiceMBean.java
@@ -26,6 +26,8 @@ import org.apache.commons.logging.LogFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jboss.ejb3.annotation.Service;
 
+import be.fedict.eid.applet.service.signer.ooxml.OOXMLProvider;
+
 /**
  * MBean to manage some runtime aspects like registering security providers.
  * 
@@ -39,7 +41,9 @@ public class DigitalSignatureServiceMBean implements
 	private static final Log LOG = LogFactory
 			.getLog(DigitalSignatureServiceMBean.class);
 
-	private Provider managedProvider;
+	private Provider managedBouncyCastleProvider;
+
+	private Provider managedOOXMLProvider;
 
 	public void create() throws Exception {
 		LOG.debug("create");
@@ -47,22 +51,54 @@ public class DigitalSignatureServiceMBean implements
 
 	public void start() throws Exception {
 		LOG.debug("start");
+		registerBouncyCastle();
+		registerOOXMLProvider();
+	}
+
+	private void registerOOXMLProvider() {
+		Provider provider = Security.getProvider(OOXMLProvider.NAME);
+		if (null != provider) {
+			LOG.debug("we don't register OOXMLProvider");
+			return;
+		}
+		this.managedOOXMLProvider = new OOXMLProvider();
+		LOG.debug("we register OOXMLProvider");
+		if (-1 == Security.addProvider(this.managedOOXMLProvider)) {
+			LOG.fatal("could not register OOXMLProvider");
+		}
+	}
+
+	private void registerBouncyCastle() {
 		Provider provider = Security
 				.getProvider(BouncyCastleProvider.PROVIDER_NAME);
 		if (null != provider) {
 			LOG.debug("we don't register BouncyCastle");
 			return;
 		}
-		this.managedProvider = new BouncyCastleProvider();
+		this.managedBouncyCastleProvider = new BouncyCastleProvider();
 		LOG.debug("we register BouncyCastle");
-		if (-1 == Security.addProvider(this.managedProvider)) {
+		if (-1 == Security.addProvider(this.managedBouncyCastleProvider)) {
 			LOG.fatal("could not register BouncyCastle");
 		}
 	}
 
 	public void stop() {
 		LOG.debug("stop");
-		if (null == this.managedProvider) {
+		unregisterBouncyCastle();
+		unregisterOOXMLProvider();
+	}
+
+	private void unregisterOOXMLProvider() {
+		if (null == this.managedOOXMLProvider) {
+			LOG.debug("we don't unregister OOXMLProvider");
+			return;
+		}
+		LOG.debug("we unregister OOXMLProvider");
+		Security.removeProvider(OOXMLProvider.NAME);
+	}
+
+	private void unregisterBouncyCastle() {
+		if (null == this.managedBouncyCastleProvider) {
 			LOG.debug("we don't unregister BouncyCastle");
 			return;
 		}
